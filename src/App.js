bd365d3f2d5a1897d065f0cfae562844c0249ab4
sero-98 <aroni.carbajals@gmail.com>
2021-10-24 18:15:12 -0500

    ttt

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const [api, setApi] = useState({ respuesta: 'no te encontre'});
+  
+  useEffect(() => {
+    async function consultaAPI(){
+      const consulta = await axios.get('/ttt');
+      console.log(consulta);
+      setApi(consulta.data);
+    };
+ 
+    consultaAPI();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>hola</h1>
+      <h1>{api.respuesta}</h1>
+    </div>
+  );
+}
+
+export default App;
