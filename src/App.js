2869d4f4d3f4b55eed48a4c36c9b2bc006d9a7b5
sero-98 <aroni.carbajals@gmail.com>
2021-10-26 22:35:52 -0500

    creando browser

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Seed from './components/seed.js';
 
 function App() {
-
-  const [api, setApi] = useState({ respuesta: 'no te encontre'});
-  
-  useEffect(() => {
-    async function consultaAPI(){
-      const consulta = await axios({
-        method: 'GET',
-        url: 'https://backend-ciro.herokuapp.com/ttt'
-      });
-      console.log(consulta);
-      setApi(consulta.data);
-    };
- 
-    consultaAPI();
-  }, []);
-
   return (
-    <div className="App">
-      <h1>hola</h1>
-      <h1>{api.respuesta}</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/seed" component={Seed} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
